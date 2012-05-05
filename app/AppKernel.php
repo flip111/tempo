752aa647892fe480432768d4780d274e7f85d5db
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2012-05-05 14:22:13 +0200

    dd files for sandbox

---
+++ app/AppKernel.php
@@ -0,0 +1,48 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel {
+
+    public function registerBundles() {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Leek\GitDebugBundle\LeekGitDebugBundle();
+        }
+
+        $bundles[] = new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle();
+
+        $bundles[] = new FOS\UserBundle\FOSUserBundle();
+
+        $bundles[] = new \Ikimea\CKeditorBundle\IkimeaCKEditorBundle();
+        
+        $bundles[] = new Sonata\SeoBundle\SonataSeoBundle();
+
+        //CRUM
+        $bundles[] = new Tempo\MainBundle\TempoMainBundle();
+        $bundles[] = new Tempo\UserBundle\TempoUserBundle();
+        $bundles[] = new Tempo\ProjectBundle\TempoProjectBundle();
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader) {
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
+
+}
