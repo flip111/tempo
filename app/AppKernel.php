d852cee130e4b4d93f06a39e943e5701720d7fed
Mlanawo Mbechezi <mlanawo.mbechezi@ikimea.com>
2012-05-25 15:31:30 +0200

    update security

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,8 +27,6 @@ class AppKernel extends Kernel {
             $bundles[] = new Leek\GitDebugBundle\LeekGitDebugBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
             $bundles[] = new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle();
-
-            
         }
 
         $bundles[] = new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle();
