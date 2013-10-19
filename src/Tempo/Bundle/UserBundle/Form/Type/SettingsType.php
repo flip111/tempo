4895591a4663b20b468129fcaccc780ad4744252
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2013-10-19 14:54:14 +0200

    add files

---
+++ src/Tempo/Bundle/UserBundle/Form/Type/SettingsType.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo.ikimea.com/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Bundle\UserBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormViewInterface;
+
+
+class SettingsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('lang', 'text', array());
+    }
+
+    public function getName()
+    {
+        return 'setting';
+    }
+}
