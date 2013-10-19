fc208320fbfd79bbef943b16e676a1ed5a5d5214
Jérémy LEHERPEUR <jeremy@leherpeur.net>
2013-10-20 00:23:09 +0200

    Load providers configuration

--- src/Tempo/Bundle/ActivityBundle/DependencyInjection/TempoActivityExtension.php
+++ src/Tempo/Bundle/ActivityBundle/DependencyInjection/TempoActivityExtension.php
@@ -24,5 +24,6 @@ class TempoActivityExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('providers.yml');
     }
 }
