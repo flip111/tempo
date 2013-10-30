dbc8a90d06a1aee056b3f9e386deefc6823cf9f1
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2013-10-30 02:32:15 +0100

    added model.

---
+++ src/Tempo/Bundle/ProjectBundle/Model/TeamInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo.ikimea.com/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Bundle\ProjectBundle\Model;
+
+/**
+ * @author Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
+ */
+
+interface TeamInterface
+{
+
+}
