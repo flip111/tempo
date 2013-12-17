83c34526c4b04ccc34789b901703aa8b83b2aae7
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2013-12-17 16:29:42 +0100

    [Debug] fixed tests

--- web/app_dev.php
+++ web/app_dev.php
@@ -6,7 +6,7 @@
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
+/*if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
@@ -16,7 +16,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
-}
+}*/
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
