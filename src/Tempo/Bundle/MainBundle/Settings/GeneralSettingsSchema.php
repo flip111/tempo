fdbdea5b72759b74c816e13cedbca5b71c274c1e
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-01-20 17:29:46 +0100

    fails to file

---
+++ src/Tempo/Bundle/MainBundle/Settings/GeneralSettingsSchema.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+* This file is part of the Tempo-project package http://tempo.ikimea.com/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Tempo\Bundle\MainBundle\Settings;
+
+use Sylius\Bundle\SettingsBundle\Schema\SchemaInterface;
+use Sylius\Bundle\SettingsBundle\Schema\SettingsBuilderInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class GeneralSettingsSchema implements SchemaInterface
+{
+    public function buildSettings(SettingsBuilderInterface $builder)
+    {
+        $builder
+            ->setDefaults(array(
+                'name'            => 'Tempo',
+            ))
+            ->setAllowedTypes(array(
+                'name'            => array('string'),
+            ))
+        ;
+    }
+
+    public function buildForm(FormBuilderInterface $builder)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+}
