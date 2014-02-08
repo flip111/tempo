72ff5874d560b82e5fe21a83b36d9584f50f211f
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-02-08 04:12:28 +0100

    phpdoc

--- src/Tempo/Bundle/ProjectBundle/Tabs/SettingsTab.php
+++ src/Tempo/Bundle/ProjectBundle/Tabs/SettingsTab.php
@@ -1,19 +1,37 @@
 <?php
 
+/*
+* This file is part of the Tempo-project package http://tempo.ikimea.com/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Tempo\Bundle\ProjectBundle\Tabs;
 
 class SettingsTab implements TabProviderInterface
 {
+    /**
+     * {@inheritdoc}
+     */
     public function getId()
     {
         return "settings";
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return "Settings";
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getContent()
     {
         return "TempoProjectBundle:Project/Tabs:settings.html.twig";
--- src/Tempo/Bundle/ProjectBundle/Tabs/TabProviderInterface.php
+++ src/Tempo/Bundle/ProjectBundle/Tabs/TabProviderInterface.php
@@ -1,10 +1,30 @@
 <?php
 
+/*
+* This file is part of the Tempo-project package http://tempo.ikimea.com/>.
+*
+* (c) Mlanawo Mbechezi  <mlanawo.mbechezi@ikimea.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Tempo\Bundle\ProjectBundle\Tabs;
 
 interface TabProviderInterface
 {
+    /**
+     * @return integer $id
+     */
     public function getId();
+
+    /**
+     * @return string $name
+     */
     public function getName();
+
+    /**
+     * @return string $content
+     */
     public function getContent();
 }
