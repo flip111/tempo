6b69bdaace508f34aa49694d7d9ba92c8f597b0c
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-03-24 19:47:16 +0100

    Refactor Behat Context. @Taluu thank you for teaching me life

--- features/frontend/project/dashboard.feature
+++ features/frontend/project/dashboard.feature
@@ -5,7 +5,7 @@ Feature: dashboard project
     Given I am connected as "admin"
 
   Scenario: Viewing the dashboard project
-    When I go to "project"
+    When I am on route "project_home"
 
     And I should see "Select an organization to view its projects"
     And I should see "Ikimea"
--- src/Tempo/Bundle/MainBundle/Behat/FeatureContext.php
+++ src/Tempo/Bundle/MainBundle/Behat/FeatureContext.php
@@ -12,21 +12,18 @@
 
 namespace Tempo\Bundle\MainBundle\Behat;
 
-use Behat\Behat\Context\Step;
-use Behat\Behat\Context\Step\When;
 use Behat\Symfony2Extension\Context\KernelAwareInterface;
-use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-use Behat\MinkExtension\Context\RawMinkContext;
+use Behat\MinkExtension\Context\MinkContext;
 
 
 
 /**
  * Features context.
  */
-class FeatureContext extends RawMinkContext implements KernelAwareInterface
+class FeatureContext extends MinkContext implements KernelAwareInterface
 {
 
     /**
@@ -48,6 +45,8 @@ class FeatureContext extends RawMinkContext implements KernelAwareInterface
     public function __construct(array $parameters)
     {
         $this->useContext('web-user', new WebUser());
+        $this->useContext('data', new DataContext());
+
 
         Request::enableHttpMethodParameterOverride();
     }
@@ -61,12 +60,152 @@ class FeatureContext extends RawMinkContext implements KernelAwareInterface
     }
 
     /**
+     * Get service by id.
+     *
+     * @param string $id
+     *
+     * @return object
+     */
+    private function getService($id)
+    {
+        return $this->getContainer()->get($id);
+    }
+
+    /**
+     * Returns Container instance.
+     *
+     * @return ContainerInterface
+     */
+    protected function getContainer()
+    {
+        return $this->kernel->getContainer();
+    }
+
+    /**
+     * Generate url.
+     *
+     * @param string  $route
+     * @param array   $parameters
+     * @param Boolean $absolute
+     *
+     * @return string
+     */
+    public function generateUrl($route, array $parameters = array(), $absolute = false)
+    {
+        return $this->getService('router')->generate($route, $parameters, $absolute);
+    }
+
+    /**
+     * Generate page url from name and parameters.
+     *
+     * @param string $page
+     * @param array  $parameters
+     *
+     * @return string
+     */
+    public function generatePageUrl($page, array $parameters = array())
+    {
+        $parts = explode(' ', trim($page), 2);
+        if (2 === count($parts)) {
+            $parts[1] = Inflector::camelize($parts[1]);
+        }
+
+        $route  = implode('_', $parts);
+        $routes = $this->getContainer()->get('router')->getRouteCollection();
+
+        if (null === $routes->get($route)) {
+            $route = 'app_'.$route;
+        }
+
+        return $this->getMinkParameter('base_url').$this->generateUrl($route, $parameters);
+    }
+
+    /**
      * @Given /^I am on route "([^"]*)"$/
      */
     public function iAmOnRoute($route)
     {
-        $route = $this->kernel->getContainer()->get('router')->generate($route, array(), false);
-        $this->getSession()->visit($this->getMinkParameter('base_url').$route);
+        $url = $this->kernel->getContainer()->get('router')->generate($route, array(), false);
+        $this->getSession()->visit($this->locatePath($url));
+    }
+
+    /**
+     * Wait
+     *
+     * @param integer $time
+     * @param string  $condition
+     *
+     * @throws BehaviorException If timeout is reached
+     */
+    public function wait($time = 10000, $condition = null)
+    {
+        if (!$this->getSession()->getDriver() instanceof Selenium2Driver) {
+            return;
+        }
+
+        $start = microtime(true);
+        $end = $start + $time / 1000.0;
+
+        $condition = $condition !== null ? $condition : <<<JS
+        document.readyState == 'complete'                  // Page is ready
+            && typeof $ != 'undefined'                     // jQuery is loaded
+            && !$.active                                   // No ajax request is active
+            && $('#page').css('display') == 'block'        // Page is displayed (no progress bar)
+            && $('.loading-mask').css('display') == 'none' // Page is not loading (no black mask loading page)
+            && $('.jstree-loading').length == 0;           // Jstree has finished loading
+JS;
+
+        // Make sure the AJAX calls are fired up before checking the condition
+        $this->getSession()->wait(100, false);
+
+        $this->getSession()->wait($time, $condition);
+
+        // Check if we reached the timeout unless the condition is false to explicitly wait the specified time
+        if ($condition !== false && microtime(true) > $end) {
+            throw new BehaviorException(sprintf('Timeout of %d reached when checking on %s', $time, $condition));
+        }
+    }
+    /**
+     * Click on the element with the provided xpath query
+     *
+     * @When /^I click on the element with xpath "([^"]*)"$/
+     */
+    public function iClickOnTheElementWithXPath($xpath)
+    {
+        $session = $this->getSession(); // get the mink session
+        $element = $session->getPage()->find(
+            'xpath',
+            $session->getSelectorsHandler()->selectorToXpath('xpath', $xpath)
+        ); // runs the actual query and returns the element
+
+        // errors must not pass silently
+        if (null === $element) {
+            throw new \InvalidArgumentException(sprintf('Could not evaluate XPath: "%s"', $xpath));
+        }
+
+        // ok, let's click on it
+        $element->click();
+
+    }
+
+    /**
+     * Click on the element with the provided CSS Selector
+     *
+     * @When /^I click on the element with css selector "([^"]*)"$/
+     */
+    public function iClickOnTheElementWithCSSSelector($cssSelector)
+    {
+        $session = $this->getSession();
+        $element = $session->getPage()->find(
+            'xpath',
+            $session->getSelectorsHandler()->selectorToXpath('css', $cssSelector) // just changed xpath to css
+        );
+        if (null === $element) {
+            throw new \InvalidArgumentException(sprintf('Could not evaluate CSS Selector: "%s"', $cssSelector));
+        }
+
+        $element->click();
+
     }
 
     /**
